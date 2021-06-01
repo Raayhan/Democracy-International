631df6510d65a0f2f658bb63b2fdb176f0ee2ab4
Raayhan <rayhan.rakib@northsouth.edu>
2021-06-01 21:13:54 +0600

    Initialized Base project with Auth

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('pagetitle', 'Welcome')
+@section('content')
+<div class="container front-panel py-5">
+    <div class="row justify-content-center">
+        <div class="col-md animated zoomIn text-center text-secondary" style="margin-top:5%">
+            <h6>Welcome to</h6>
+            <div class="row justify-content-center">
+                <h1 class="brown-text">Democracy International</h1>
+            </div>
+          
+            <p style="margin-top:5%">Bangladesh Democratic Participation and Reform Program</p>
+            <div class="row justify-content-center mb-5">
+                <button onclick="window.location.href='/login'" class="btn btn-primary">GET STARTED</button>
+               
+            </div>
+        </div>
+     
+    </div>
+</div>
+
+@endsection
