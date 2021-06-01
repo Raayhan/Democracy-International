c802fdb5e950c1c123fdf352f9b4148a45ed7991
Raayhan <rayhan.rakib@northsouth.edu>
2021-06-01 21:38:06 +0600

    redesigned the Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,34 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header text-center">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                  <h2 class="text-center mb-5">Welcome</h2>
+                  <div class="row">
+                      <div class="col md-6">
+                        <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title">Add Party</h5>
+                              <p class="card-text">
+                                Add a new party to the system.
+                              </p>
+                              <button type="button" class="btn btn-unique btn-sm">Go</button>
+                            </div>
+                          </div>
+                      </div>
+                      <div class="col md-6">
+                        <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title">Create Memeber</h5>
+                              <p class="card-text">
+                                Add a new member to a party.
+                              </p>
+                              <button type="button" class="btn btn-unique btn-sm">Go</button>
+                            </div>
+                          </div>
+                      </div>
+                  </div>
                 </div>
             </div>
         </div>
